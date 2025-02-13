8c82a10d23e7ffb8f434aba0c07253e46799a98d
HyperPaint <dioman172@ya.ru>
2025-02-13 22:19:13 +1000

    openvpn=2.6.12-r1

---
+++ openvpn-client/files/root/scripts/prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source "/root/scripts/hbdl/log.sh"
+
+you_need_run_container_in_privileged_mode="You need run container in privileged mode"
+
+if [[ -d "/dev/net" ]]; then
+  log "/dev/net found"
+else
+  mkdir "/dev/net" || { error "$you_need_run_container_in_privileged_mode"; exit 1; }
+  log "/dev/net created"
+fi
+
+if [[ -c "/dev/net/tun" ]]; then
+  log "/dev/net/tun found"
+else
+  mknod /dev/net/tun c 10 200 || { error "$you_need_run_container_in_privileged_mode"; exit 1; }
+  log "/dev/net/tun created"
+fi
--- openvpn-server/docker-build.sh
+++ openvpn-server/docker-build.sh
@@ -3,6 +3,6 @@
 author="hyperpaint"
 name="openvpn-server"
 build_version="1.0.0"
-openvpn_version="2.6"
+openvpn_version="2.6.12-r1"
 
 docker build -t "$author/$name:$build_version-$openvpn_version" .
