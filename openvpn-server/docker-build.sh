ef1a9b62fcec620994e4bfe8b3aa9423dd5cc415
HyperPaint <dioman172@ya.ru>
2025-04-06 22:37:30 +1000

    UP

--- openvpn-server/docker-build.sh
+++ openvpn-server/docker-build.sh
@@ -2,7 +2,7 @@
 
 author="hyperpaint"
 name="openvpn-server"
-build_version="1.0.0"
+build_version="1.1.0"
 openvpn_version="2.6.12-r1"
 
 docker build -t "$author/$name:$build_version-$openvpn_version" .
