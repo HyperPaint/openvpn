35d1a3c7ae7114284324fca39922dc29a5a63ed0
HyperPaint <dioman172@ya.ru>
2026-03-18 23:19:33 +1000

    1.2.0+2.6.16-r0

--- openvpn-client/docker-build.sh
+++ openvpn-client/docker-build.sh
@@ -3,6 +3,6 @@
 author="hyperpaint"
 name="openvpn-client"
 build_version="1.0.0"
-openvpn_version="2.6.14-r0"
+openvpn_version="2.6.16-r0"
 
 docker build -t "$author/$name:$build_version-$openvpn_version" .
--- openvpn-server/docker-build.sh
+++ openvpn-server/docker-build.sh
@@ -3,6 +3,6 @@
 author="hyperpaint"
 name="openvpn-server"
 build_version="1.2.0"
-openvpn_version="2.6.14-r0"
+openvpn_version="2.6.16-r0"
 
 docker build -t "$author/$name:$build_version-$openvpn_version" .
